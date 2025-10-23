db5f47800488c4e8eb4d5a50bd0e0e6025dd787e
Frederico Bede <bede.apps@gmail.com>
2025-10-23 20:53:59 +0000

    [BAI-70] - Finishing vault configuration

--- resources/vault-scripts/configure-policies.sh
+++ resources/vault-scripts/configure-policies.sh
@@ -6,17 +6,17 @@ auth='auth-manager'
 issuer='issuer-manager'
 policy='policy-manager'
 
-cat <<EOF | vault policy write $auth --no-print -
+cat <<EOF | vault policy write $auth - > /dev/null 2>&1
 path "sys/auth/*" { capabilities = ["create", "update", "sudo"] }
 path "sys/auth"   { capabilities = ["read"] }
 path "auth/*"     { capabilities = [ "update", "create" ] }
 EOF
 
-cat <<EOF | vault policy write $policy --no-print -
+cat <<EOF | vault policy write $policy - > /dev/null 2>&1
 path "sys/policies/*" { capabilities = [ "create", "update", "read", "list" ] }
 EOF
 
-cat <<EOF | vault policy write $issuer --no-print -
+cat <<EOF | vault policy write $issuer - > /dev/null 2>&1
 path "sys/mounts/*" { capabilities = [ "create", "read", "update", "delete", "list" ] }
 path "sys/mounts" { capabilities = [ "read", "list" ] }
 path "pki*" { capabilities = [ "create", "read", "update", "delete", "list", "sudo", "patch" ] }
