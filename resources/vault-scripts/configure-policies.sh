910bc6fcd3ca9609527bcf64e08e5625eccbc83a
Frederico Bede <bede.apps@gmail.com>
2025-10-23 12:59:34 +0000

    [BAI-70] - Testing vault scripts

--- resources/vault-scripts/configure-policies.sh
+++ resources/vault-scripts/configure-policies.sh
@@ -6,17 +6,17 @@ auth='auth-manager'
 issuer='issuer-manager'
 policy='policy-manager'
 
-cat <<EOF | vault policy write $auth --no-print  -
+cat <<EOF | vault policy write $auth --no-print -
 path "sys/auth/*" { capabilities = ["create", "update", "sudo"] }
 path "sys/auth"   { capabilities = ["read"] }
 path "auth/*"     { capabilities = [ "update", "create" ] }
 EOF
 
-cat <<EOF | vault policy write $policy -
+cat <<EOF | vault policy write $policy --no-print -
 path "sys/policies/*" { capabilities = [ "create", "update", "read", "list" ] }
 EOF
 
-cat <<EOF | vault policy write $issuer -
+cat <<EOF | vault policy write $issuer --no-print -
 path "sys/mounts/*" { capabilities = [ "create", "read", "update", "delete", "list" ] }
 path "sys/mounts" { capabilities = [ "read", "list" ] }
 path "pki*" { capabilities = [ "create", "read", "update", "delete", "list", "sudo", "patch" ] }
