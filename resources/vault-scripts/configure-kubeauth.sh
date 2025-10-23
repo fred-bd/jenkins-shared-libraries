5d8478762e5159fc7bf44374edfd1d08a07373a2
Frederico Bede <bede.apps@gmail.com>
2025-10-23 14:51:53 +0000

    [BAI-70] - Testing vault scripts

--- resources/vault-scripts/configure-kubeauth.sh
+++ resources/vault-scripts/configure-kubeauth.sh
@@ -5,22 +5,22 @@ APPROLE_NAME="kubeauth"
 APPROLE_EXISTS=$(vault auth list | grep "$APPROLE_NAME" || true)
 
 if [ -z "$APPROLE_EXISTS" ]; then
-  vault auth enable --no-print -path="$APPROLE_NAME" approle
+  vault auth enable -path="$APPROLE_NAME" approle --no-print
 else
   echo "!!$APPROLE_NAME already exists!!"
 fi
 
 policies="$POLICIES"
 
-vault write auth/$APPROLE_NAME/role/configurer --no-print \
+vault write auth/$APPROLE_NAME/role/configurer \
   token_policies=$policies \
   secret_id_ttl=0 \
   token_num_uses=0 \
   token_ttl=4h \
   token_max_ttl=8h \
-  secret_id_num_uses=0
+  secret_id_num_uses=0  --no-print
 
 approle_id=$(vault read auth/$APPROLE_NAME/role/configurer/role-id -format=json | jq -r .data.role_id)
-secret_id=$(vault write --no-print -force auth/$APPROLE_NAME/role/configurer/secret-id -format=json | jq -r .data.secret_id)
+secret_id=$(vault write -force auth/$APPROLE_NAME/role/configurer/secret-id -format=json | jq -r .data.secret_id)
 
 echo "id=$approle_id,secret=$secret_id" 
