f9365b2933eb0cfa2a4c179361b3662c4bb9bc9e
Frederico Bede <bede.apps@gmail.com>
2025-10-24 15:13:41 +0000

    [BAI-71] - Fixing flux apps repos

--- resources/flux-scripts/clean-cluster.sh
+++ resources/flux-scripts/clean-cluster.sh
@@ -11,6 +11,8 @@ kubectl delete --all Kustomization -A --ignore-not-found
 kubectl wait --for=delete --all Kustomization -n flux-system
 
 flux uninstall --silent
+
+kubectl delete ns flux-system  --ignore-not-found
 kubectl wait --for=delete namespace/flux-system --timeout=60s --ignore-not-found
 
 kubectl delete ns vault  --ignore-not-found
