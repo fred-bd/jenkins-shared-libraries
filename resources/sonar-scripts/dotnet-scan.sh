ab487e2b1c974576c7eda92178767ac295d1025b
Frederico Bede <bede.apps@gmail.com>
2025-11-20 01:29:22 +0000

    [BAI-84] - Unittests

--- resources/sonar-scripts/dotnet-scan.sh
+++ resources/sonar-scripts/dotnet-scan.sh
@@ -36,7 +36,7 @@ ls -la $PWD/unittests
 
 coverlet $OUT_DIR/unittests.dll \
     --target "dotnet" \
-    --targetargs "test $PWD/unittests/unittests.csproj --no-build " \
+    --targetargs "test $PWD/unittests --no-build " \
     -f=opencover \
     -o="coverage.xml" --verbosity detailed
 
