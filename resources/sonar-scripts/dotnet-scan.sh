97e21e4e2751fae6e669a60eeb80fe386dba6d1c
Frederico Bede <bede.apps@gmail.com>
2025-11-20 01:49:46 +0000

    [BAI-84] - Unittests

--- resources/sonar-scripts/dotnet-scan.sh
+++ resources/sonar-scripts/dotnet-scan.sh
@@ -1,43 +1,18 @@
 #!/bin/bash
 
-# PROJECT_ID=
-# ACCESS_TOKEN=
-# SONAR_URL=
-# BRANCH_NAME=
-
-# env
-# echo '------'
-# echo "ls -la"
-# echo "PROJECT_ID=[$PROJECT_ID]"
-# echo "ACCESS_TOKEN=[$ACCESS_TOKEN]"
-# echo "SONAR_URL=[$SONAR_URL]"
-# echo "BRANCH_NAME=[$BRANCH_NAME]"
-
-# ls -la
-
 OUT_DIR=$(dotnet msbuild unittests -nologo -t:Build -property:Configuration=Debug -getProperty:TargetDir)
 
-dotnet sonarscanner begin /k:"$PROJECT_ID" \
+dotnet sonarscanner begin /k:"$PROJECT_ID-$BRANCH_NAME" \
     /d:sonar.token="$ACCESS_TOKEN" \
     /d:sonar.cs.opencover.reportsPaths=coverage.xml \
     /d:sonar.host.url="$SONAR_URL" 
-    # \
-    # /d:sonar.pullrequest.branch="$BRANCH_NAME" \
-    # /d:sonar.pullrequest.base="main"
 
-# dotnet build -c Release -o $OUT_DIR --no-incremental
 dotnet build --no-incremental
-
-# echo '----'
-# echo "out: $OUT_DIR"
-# ls -la $OUT_DIR
-# echo '----'
-# ls -la $PWD/unittests
 
 coverlet $OUT_DIR/unittests.dll \
     --target "dotnet" \
-    --targetargs "test flux-micro-svc.sln --no-build " \
+    --targetargs "test --no-build " \
     -f=opencover \
-    -o="coverage.xml" --verbosity detailed
+    -o="coverage.xml" 
 
 dotnet sonarscanner end /d:sonar.token="$ACCESS_TOKEN"
